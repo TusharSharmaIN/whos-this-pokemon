3b64f9ce742132be8638fc7d608612c48b56be5c
Tushar Sharma <tushars500@gmail.com>
2024-04-24 19:15:30 +0530

    initial changes

---
+++ script.js
@@ -0,0 +1,65 @@
+const enterButton = document.getElementById("enter");
+const reloadButton = document.getElementById("reload");
+const audioPokemon = document.getElementById("audiopokemon");
+
+var pokemonName = "";
+var pokemonImageUrl = "";
+
+function getRandomPokemon() {
+  //  considering only kanto region pokemon
+  const pokeLength = 151;
+  const randPokemonId = Math.floor(Math.random() * pokeLength);
+  fetch(`https://pokeapi.co/api/v2/pokemon-form/${randPokemonId}/`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      pokemonName = data["pokemon"]["name"];
+      pokemonImageUrl = data["sprites"]["front_default"];
+      const img = document.querySelector("img");
+      img.setAttribute("src", pokemonImageUrl);
+      console.log("POKEMON: " + pokemonName);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+onReset = () => {
+  document.querySelector("span").innerHTML = " ";
+  document.getElementById("imgpokemon").className = null;
+  audioPokemon.play();
+  getRandomPokemon();
+};
+
+onEnter = () => {
+  let response = document.querySelector("input").value.trim();
+  const span = document.querySelector("span");
+  const imgPokemon = document.getElementById("imgpokemon");
+  const input = document.querySelector("input");
+
+  if (response.length) {
+    if (response.toLowerCase() == pokemonName.toLowerCase()) {
+      span.innerHTML = "Congratulations! It's " + pokemonName;
+      imgPokemon.className = "unhide";
+      input.value = "";
+      setTimeout(onReset, 1500);
+    } else {
+      span.innerHTML = "You missed! It's " + pokemonName.toLowerCase();
+      imgPokemon.className = "unhide";
+      input.value = "";
+      setTimeout(onReset, 1500);
+    }
+  } else {
+    span.innerHTML = "Enter the name";
+  }
+};
+
+enterButton.addEventListener("click", onEnter);
+
+reloadButton.addEventListener("click", onReset);
+
+getRandomPokemon();
